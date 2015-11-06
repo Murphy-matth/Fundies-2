a9cdef58ce6f9bf719af5104d8c4da2b5f27edeb
Murphy-matth <murphy1022@gmail.com>
2015-11-06 13:43:44 -0500

    Update Visitor.java

--- Assignment-6/Visitor.java
+++ Assignment-6/Visitor.java
@@ -73,6 +73,32 @@ class DoubleVisitor implements IArithVisitor<IArith> {
 		return new Formula(that.fun, that.name, that.left.accept(this), that.right.accept(this));
 	}
 }
+class AllSmallVisitor implements IArithVisitor<Boolean> {
+	public Boolean visitConst(Const that) {
+		return that.num < 10;
+	}
+	public Boolean visitFormula(Formula that) {
+		return that.left.accept(this) && that.right.accept(this);
+	}
+}
+class NoDivBy0 implements IArithVisitor<Boolean> {
+	public Boolean visitConst(Const that) {
+		return that.num > .0001 || that.num < -.001;
+	}
+	public Boolean visitFormula(Formula that) {
+		if (that.name == "div") {
+			if(!that.right.accept(this)) {
+				return false;
+			}
+			else {
+				return that.left.accept(this) && that.right.accept(this);
+			}
+		}
+		else {
+			return that.left.accept(this) && that.right.accept(this);
+		}
+	}
+}
 
 class func implements IFunc2<Double, Double, Double> {
 	public Double apply(Double a1, Double a2) {
@@ -86,6 +112,8 @@ class ExampleVisitor {
 	
 	IArith IAriath1 = new Formula(new func(), "Matt", new Const(10), new Const(12));
 	IArith IArith2 = new Formula(new func(), "Matt", new Const(20), new Const(24));
+	IArith IArith3 = new Formula(new func(), "Matt", new Const(2), new Const(4));
+	IArith IArith4 = new Formula(new func(), "div", new Const(2), new Const(0));
 	
 	
 	boolean test(Tester t) {
@@ -96,5 +124,13 @@ class ExampleVisitor {
 	}
 	boolean testDouble(Tester t) {
 		return t.checkExpect(this.IAriath1.accept(new DoubleVisitor() ), IArith2);
+	}
+	boolean testSmall(Tester t) {
+		return t.checkExpect(this.IAriath1.accept(new AllSmallVisitor() ), false) &&
+				t.checkExpect(this.IArith3.accept(new AllSmallVisitor() ), true);
+	}
+	boolean testDiv(Tester t) {
+		return t.checkExpect(this.IAriath1.accept(new NoDivBy0() ), true) &&
+				t.checkExpect(this.IArith4.accept(new NoDivBy0() ), false);
 	}
 }
