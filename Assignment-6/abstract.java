64ce854dbed638e8d0b4bccd5c56bf8780e9f415
Murphy-matth <murphy1022@gmail.com>
2015-11-06 11:16:23 -0500

    Update abstract.java

--- Assignment-6/abstract.java
+++ Assignment-6/abstract.java
@@ -3,8 +3,64 @@ public class ab {
 
 }
 
-interface IComparator<T> {
+interface IPred <T> {
+	boolean apply(T arg);
+}
+
+interface IComparator <T> {
 	int compare(T t1, T t2);
+}
+
+interface IList <T> {
+	IList<T> filter(IPred<T>pred);
+	IList<T> sort(IComparator<T> comp);
+	IList<T> insertBy(IComparator<T> comp, T t);
+	int length();
+}
+
+
+class MtList<T> implements IList<T> {
+	public IList<T> filter(IPred<T> pred) { return this; }
+    public IList<T> sort(IComparator<T> comp) { return this; }
+    public IList<T> insertBy(IComparator<T> comp, T t) {
+    	return new ConsList<T>(t, this);
+    }
+	public int length() { return 0; }
+}
+
+class ConsList<T> implements IList<T> {
+	T first;
+	IList<T> rest;
+	ConsList(T first, IList<T> rest) {
+		this.first = first;
+		this.rest = rest;
+	}
+	
+	public int length() {
+		return 1 + rest.length();
+	}
+	
+	public IList<T> filter(IPred<T> pred) {
+		if (pred.apply(this.first)) {
+			return new ConsList<T>(this.first, this.rest.filter(pred));
+		}
+		else {
+			return this.rest.filter(pred);
+		}
+	}
+	
+	public IList<T> sort(IComparator<T> comp) {
+		return this.rest.sort(comp).insertBy(comp, this.first);
+	}
+	
+	public IList<T> insertBy(IComparator<T> comp, T t) {
+		if (comp.compare(this.first, t) < 0) {
+			return new ConsList<T>(this.first, this.rest.insertBy(comp, t));
+		}
+		else {
+			return new ConsList<T>(t, this);
+		}
+	}
 }
 
 abstract class ABST<T> {
@@ -26,6 +82,7 @@ abstract class ABST<T> {
 		return false;
 	}
 	abstract boolean sameData(ABST<T> that);
+	abstract IList<T> buildList(IList<T> that);
 }
 
 
@@ -60,6 +117,10 @@ class Leaf<T> extends ABST<T> {
 	
 	public boolean sameData(ABST<T> that) {
 		return that.sameLeaf(this);
+	}
+	
+	public IList<T> buildList(IList<T> that) {
+		return that;
 	}
 }
 
@@ -128,6 +189,9 @@ class Node<T> extends ABST<T> {
 		}
 	}
 	
+	public IList<T> buildList(IList<T> that) {
+		return this.getRight().buildList(new ConsList<T>(this.getLeftmost(), that));
+	}
 }
 
 class Book {
